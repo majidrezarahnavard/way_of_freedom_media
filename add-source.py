b607daedc19535f5fa9cacbabbb9fdcb279939e8
Freedom Guard <118625619+Freedom-Guard-Net@users.noreply.github.com>
2025-02-12 16:22:03 +0000

    starting...

---
+++ add-source.py
@@ -0,0 +1,8 @@
+# inputs
+typeS = input("enter type source[video, audio, image]:").lower()
+nameFile = input("enter file(path)[image.png, video.mp4, audio.mp3]")
+nameFileAfter = input("enter name file:")
+description = input("enter description for file:")
+title = input("enter title for file:")
+sourcePath = "https://raw.githubusercontent.com/majidrezarahnavard/way_of_freedom_media/main/source"
+
